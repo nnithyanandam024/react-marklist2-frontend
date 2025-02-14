5639e4da009dc738ac6593ab4cba26fd0f8f3096
nnithyanandam24 <nnithyanandam24@gmail.com>
2025-02-14 08:51:54 +0530

    Updated-3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/react-marklist2-frontend'>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
